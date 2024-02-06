1759895ffb04c73fbe2b3f07e761e7287f671e3d
ferrodr <fernando.rguezm@gmail.com>
2024-02-06 17:35:46 +0100

    Backend terminado (falta reto)

--- backend/controllers/comment.js
+++ backend/controllers/comment.js
@@ -2,11 +2,10 @@ const commentRouter = require('express').Router()
 const Comment = require('../models/comment')
 
 commentRouter.get('/:game', async (request, response) => {
-    const { game } = request.body
+    const game = request.params.game;
     const comments = await Comment.find({"game": game}, { projection: { _id: 0 }}).catch(() => {
         return response.status(500).json({ error: 'Server error'})
     })
-
     response.status(200).send(comments)
 })
 
